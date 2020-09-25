a1432faa47c60e5d80223d0249b83a2878c3e001
Youareyou64 <jackasloan@gmail.com>
2020-09-25 17:06:13 -0500

    Added a full help menu system that takes arguments (parameters? idk)

--- modules/general.py
+++ modules/general.py
@@ -8,18 +8,39 @@ class General(Cog):
         self.bot = bot
 
     @command()
-    async def help(self, ctx):
-        help_embed = Embed(colour=Colour(0xff0000))
-        help_embed.set_author(name="**Scribe Bot Commands**")
-        help_embed.add_field(name="ping", value="Returns bot Ping", inline=False)
-        help_embed.add_field(name="info", value="Displays info about the bot", inline=False)
-        help_embed.add_field(name="contributors", value="Displays Bot Owner", inline=False)
-        help_embed.add_field(name="help", value="returns this message", inline=False)
-        help_embed.add_field(name="join", value="Joins a VC that you are in", inline=False)
-        help_embed.add_field(name="leave", value="leaves a VC that you are in", inline=False)
-        help_embed.add_field(name="tts", value="Speaks your message into VC", inline=False)
-        help_embed.add_field(name="nick <nickname>", value="Use command to set your TTS nickname, replace <nickname> with your desired nickname!", inline=False)
-        await ctx.send("", embed=help_embed)
+    async def help(self, ctx, *, cmd: str='general'):
+        print(cmd)
+        if cmd.lower() == 'tts':
+            await ctx.send("**TTS** | TTS can be used when you are in a voice chat and would like the bot to speak on your behalf. Simply type `s!tts [Your message here]`! Bot must be in VC to use.")
+        elif cmd.lower() == 'stt':
+            await ctx.send("STT Feature Coming Soon!")
+        elif cmd.lower() == 'nick':
+            await ctx.send("Use `s!nick [nickname]` to set a nickname that is spoken before any TTS message said on your behalf. Nicknames apply across all servers you are in.")
+        elif cmd.lower() == 'me':
+            await ctx.send("What's wrong? :/ *hugs*")
+        elif cmd.lower() == 'mute':
+            await ctx.send("Use to prevent an user from using the TTS command. Requires Mute Users server permission. Use unmute to unmute")
+        elif cmd.lower() == 'general':
+            print("general help triggered")
+            help_embed = Embed(colour=Colour(0xff0000))
+            help_embed.set_author(name="**Scribe Bot Commands**")
+            help_embed.add_field(name="ping", value="Returns bot Ping", inline=False)
+            help_embed.add_field(name="info", value="Displays info about the bot", inline=False)
+            help_embed.add_field(name="contributors", value="Displays Bot Owner", inline=False)
+            help_embed.add_field(name="help", value="returns this message", inline=False)
+            help_embed.add_field(name="join", value="Joins a VC that you are in", inline=False)
+            help_embed.add_field(name="leave", value="leaves a VC that you are in", inline=False)
+            help_embed.add_field(name="tts", value="Speaks your message into VC", inline=False)
+            help_embed.add_field(name="mute", value="Mute an user from using TTS (use unmute to unmute)", inline=False)
+            help_embed.add_field(name="nick", value="Set your TTS nickname that will be spoken before anything you say!", inline=False)
+            help_embed.add_field(name="More Help", value="**Use `s!help [command]` for more help with a command!**")
+            await ctx.send("", embed=help_embed)
+        else:
+            await ctx.send("""No advanced help found for that command. Here is a list of available advanced helps:
+            tts
+            stt
+            nick
+            mute""")
 
     @command()
     async def ping(self, ctx):
@@ -30,6 +51,10 @@ class General(Cog):
         self.bot.nicknames[str(ctx.message.author.id)] = nickname
         print(f"{self.bot.user.id}'s nickname changed to {nickname}")
         await ctx.send(":ballot_box_with_check: Nickname changed to " + nickname)
+
+    @command()
+    async def invite(self, ctx):
+        await ctx.send("As I am still in a development phase, please DM <@!435200177217732633> if you're interested in getting me in your server.")
 
     @command()
     async def info(self, ctx):
