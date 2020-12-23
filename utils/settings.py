f0075a6c5294c26bc63660f6b805d0ec1ffb407d
Youareyou64 <jackasloan@gmail.com>
2020-12-23 12:14:08 -0600

    added alt dev prefix/token support

--- utils/settings.py
+++ utils/settings.py
@@ -5,7 +5,14 @@ class Settings:
     def __init__(self):
         load_dotenv(verbose=True)
         self.default_prefix = ["s!", "<@!754137339760214097> ", "<@754137339760214097> "]
+        # uncomment for dev prefixes
+        # self.default_prefix = ["d!", "<@!791352105746300939> ", "<@791352105746300939> "]
 
+    """
+    @property
+    def discord_token(self):
+        return getenv("ALPHA_DISCORD_TOKEN")
+    """
     @property
     def discord_token(self):
         return getenv("DISCORD_TOKEN")
